d451e03d61386de9ec1aac2bd0dd361f9a675fcb
Man <man49116@donga.edu.vn>
2022-02-24 13:18:06 +0700

    home

--- flutter_realtime/lib/main.dart
+++ flutter_realtime/lib/main.dart
@@ -1,29 +1,18 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_realtime/Home.dart';
 
-import 'EditDatabase.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "user app",
-      theme: ThemeData(
-        primaryColor: Colors.greenAccent[700],
-      ),
-      home: EditDatabase(),
-      debugShowCheckedModeBanner: false,
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Home());
   }
 }
